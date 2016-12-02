a9d7300be4561c507e8fe5e5f824a14595c7c1ed
Kulyk Dmytro <lnkvisitor.ts@gmail.com>
2016-12-02 19:12:39 +0200

    Initial commit

---
+++ Support/ThemesFacade.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace RabbitCMS\Themes;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class ThemesFacade.
+ */
+class ThemesFacade extends Facade {
+
+}
---
+++ Themes.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace RabbitCMS\Themes;
+
+/**
+ * Class Themes.
+ */
+class Themes
+{
+
+}
